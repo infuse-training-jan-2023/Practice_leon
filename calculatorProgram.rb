9735fcc2edaa9e5ad6c64fc4f63d17e03600ff04
Leon <mca.2016@unigoa.com>
2023-01-22 23:25:09 +0530

    commit after reset to calculator file

--- calculatorProgram.rb
+++ calculatorProgram.rb
@@ -12,6 +12,10 @@ class Calculator
         return operand1*operand2      
     end
 
+    def div(operand1 , operand2)   
+        return operand1/operand2      
+    end
+
 end
 
 
@@ -20,3 +24,4 @@ calObj = Calculator.new
 puts "Add= "+ calObj.add(40 ,20).to_s
 puts "sub= "+ calObj.sub(40 ,20).to_s
 puts "mul= "+ calObj.mul(10 ,10).to_s
+puts "div= "+ calObj.div(20 ,2).to_s
