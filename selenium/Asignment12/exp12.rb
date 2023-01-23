5826884d1a0a306bd3deaebf000a23bd13416c89
Leon <mca.2016@unigoa.com>
2023-01-23 13:24:10 +0530

    updated assignment12

--- selenium/Asignment12/exp12.rb
+++ selenium/Asignment12/exp12.rb
@@ -1,6 +1,6 @@
 require 'selenium-webdriver'
 
-class DropDown
+class CromaWebTester
 
     attr_accessor :driver , :wait
 
@@ -11,25 +11,30 @@ class DropDown
       
     end
 
-    def perfom_action(url)
+    def openWebsite(url)
         driver.get(url) 
-        sleep(1)
+    end
+
+    def open_dropdown
         driver.find_element(:css, "span.menu-icon-hamb").click
-        sleep(1)
-         driver.find_element(:css, "ul#navigation-1 li:nth-child(7) ").click
+    end
 
+    def selectDropDownItem(index)
+        driver.find_element(:css, "ul#navigation-1 li:nth-child(#{index}) ").click
+    end
+
+    def useSearchBar(text)
         searchBar=driver.find_element(:id, "search")
-
-        searchBar.send_keys("gaming monitors 27 inch")
-
+        searchBar.send_keys(text)
         searchBar.send_key(:enter)
         sleep(2)
+    end
 
-     
-        product1= driver.find_element(:id, "245657")
-
+    def selectProduct(id)
+        product1= driver.find_element(:id, id)
         original_window = driver.window_handle
-        product1.find_element(:css ,"a").click
+        sleep(2)
+        product1.find_element(:css ,'a').click
         wait.until { driver.window_handles.length == 2 }
         driver.window_handles.each do |handle|
             if handle != original_window
@@ -38,27 +43,40 @@ class DropDown
             end
         end
         sleep(3)
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
-
+        scrollWindowBottom()
+        sleep(3)
         driver.close
-
         driver.switch_to.window original_window
+    end
 
-        offset =0
-        for a in 1..7 do
-            offset=offset+500
-            driver.execute_script("window.scrollTo(0, #{offset})")
-            sleep(0.5)
+    def scrollWindowBottom()
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+    end
+
+    def scrollWindow(offset ,loopCount , delay)
+        counter=0
+        for a in 1..loopCount do
+            counter=counter+offset
+            driver.execute_script("window.scrollTo(0, #{counter})")
+            sleep(delay)
         end
+    end
+
+    def quit()
         driver.quit
-    end   
+    end 
 end
 
-
-
 driver_path="drivers\\chromedriver_win32\\chromedriver.exe"
-exp1 =DropDown.new(driver_path)
-exp1.perfom_action("https://www.croma.com/")
+exp1 =CromaWebTester.new(driver_path)
+
+exp1.openWebsite("https://www.croma.com/")
+exp1.open_dropdown
+exp1.selectDropDownItem(7)
+exp1.useSearchBar("gaming monitors 27 inch")
+exp1.selectProduct("245657")
+exp1.scrollWindow(500 ,7 , 0.5)
+exp1.quit
 
 
 
